dcdfdf9432dee49550b163b4faef0598073c6fc6
Filip Nyquist <fillerix99@gmail.com>
2017-03-18 21:21:08 +0100

    Added XO as linter to keep stuff clean.
    Added XO as linter to keep stuff clean.

--- index.js
+++ index.js
@@ -2,34 +2,32 @@ const logger = require('winston');
 const fs = require('fs');
 const LbryTrnsf = require('./lib/LbryTrnsf');
 
-var Config = require('./lib/config');
+const Config = require('./lib/config');
 
-
-/*---LOGGING---*/
-var now = new Date();
-var t = now.toISOString().replace(/[:.]/gi, '-');
-var fname = './log/' + t + '.log';
+/* ---LOGGING--- */
+const now = new Date();
+const t = now.toISOString().replace(/[:.]/gi, '-');
+const fname = './log/' + t + '.log';
 try {
-    fs.mkdirSync('./log');
+	fs.mkdirSync('./log');
 } catch (e) {}
-
 
 logger.level = 'debug';
 
 logger.remove(
     logger.transports.Console
 ).add(logger.transports.Console, {
-    colorize: true,
-    handleExceptions: true,
-    humanReadableUnhandledException: true
+	colorize: true,
+	handleExceptions: true,
+	humanReadableUnhandledException: true
 }).add(logger.transports.File, {
-    level: 'debug',
-    filename: fname,
-    handleExceptions: true
+	level: 'debug',
+	filename: fname,
+	handleExceptions: true
 }).handleExceptions(new logger.transports.File({
-    filename: './crash.log'
+	filename: './crash.log'
 }));
 
-var config = Config();
+const config = Config();
 
-var lbryTrnsf = new LbryTrnsf(config);
+const lbryTrnsf = new LbryTrnsf(config);
